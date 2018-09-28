8d0fb4c1462bf07b88c1b2f271a7eaef35c80b50
Katie Jeffree <k.jeffree@gmail.com>
2018-09-28 11:44:13 +0100

    finished classes

--- console.rb
+++ console.rb
@@ -1,7 +1,11 @@
 # 1. class diagram √
 # 2. database diagram √
-# 3. directory structure -> git init 
-# 4. sql file
-# 5. create classes
+# 3. directory structure -> git init √
+# 4. sql file √
+# 5. create classes √
 # 6. CRUD methods
 # 7. console.rb
+
+require_relative("./models/customer.rb")
+require_relative("./models/film.rb")
+require_relative("./models/ticket.rb")
---
+++ models/ticket.rb
@@ -0,0 +1,13 @@
+require_relative("../db/sql_runner.rb")
+
+class Ticket
+
+  attr_reader :id, :customer_id, :film_id
+
+  def initialize(options)
+    @id = options["id"].to_i
+    @film_id = options["film_id"]
+    @customer_id = options["customer_id"]
+  end
+
+end
